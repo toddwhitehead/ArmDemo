8c3822d4ec327064e14cddeeaac826da9ba8cbc1
Todd Whitehead <todd@whiteheadtrust.com>
2015-05-19 23:11:36 +1000

    reviwed names

--- cross-rg-vm/create-protected-vnet/runme.ps1
+++ cross-rg-vm/create-protected-vnet/runme.ps1
@@ -1,18 +1,30 @@
 
-Select-AzureSubscription -SubscriptionID a7ba7ce4-67ef-4dc3-890e-f774571e552f 
+#login as an admin user
+Add-Azureaccount
+
+#Get details of current user accounts
+Get-AzureAccount
+
+Select-AzureSubscription -SubscriptionID 71f1f825-5296-4219-ba2c-2f7ccd5d170b
 
 Switch-AzureMode AzureResourceManager
 
-New-AzureResourceGroup -Name demo-protected-rg -Location "Southeast Asia"
+New-AzureResourceGroup -Name demo-protectedops-rg -Location "Southeast Asia"
 
-New-AzureResourceGroupDeployment -Name MyDeployment -ResourceGroupName demo-protected-rg -TemplateFile "C:\Users\towhit\Documents\GitHub\ArmDemo\cross-rg-vm\create-protected-vnet\azuredeploy.json" -TemplateParameterFile "C:\Users\towhit\Documents\GitHub\ArmDemo\cross-rg-vm\create-protected-vnet\azuredeploy.parameters.json"
+#Create the vnet
+New-AzureResourceGroupDeployment -Name MyDeployment -ResourceGroupName demo-protectedops-rg -TemplateFile "C:\Users\Todd\Documents\GitHub\ArmDemo\cross-rg-vm\create-protected-vnet\azuredeploy.json" -TemplateParameterFile "C:\Users\Todd\Documents\GitHub\ArmDemo\cross-rg-vm\create-protected-vnet\azuredeploy.parameters.json"
 
-New-AzureResourceGroupDeployment -Name MyVMDeployment -ResourceGroupName demo-protected-rg -TemplateFile "C:\Users\towhit\Documents\GitHub\ArmDemo\cross-rg-vm\azuredeploy.json" -TemplateParameterFile "C:\Users\towhit\Documents\GitHub\ArmDemo\cross-rg-vm\azuredeploy.parameters.json"
+#create the VM (as an admin)
+New-AzureResourceGroupDeployment -Name MyVMDeployment -ResourceGroupName demo-protectedops-rg -TemplateFile "C:\Users\Todd\Documents\GitHub\ArmDemo\cross-rg-vm\azuredeploy.json" -TemplateParameterFile "C:\Users\Todd\Documents\GitHub\ArmDemo\cross-rg-vm\azuredeploy.parameters.json"
 
-Remove-AzureAccount -Name todd@whiteheadtrust.com
 
+Remove-AzureAccount -Name demo@toddbert.com
+
+#login as dev account
 Add-Azureaccount
 
-New-AzureResourceGroup -Name demo-unprotected-rg -Location "Southeast Asia"
+#New-AzureResourceGroup -Name demo-unprotected-rg -Location "Southeast Asia"
 
+#create the VM (as an admin)
+New-AzureResourceGroupDeployment -Name MyVMDeployment -ResourceGroupName demo-protectedops-rg -TemplateFile "C:\Users\Todd\Documents\GitHub\ArmDemo\cross-rg-vm\azuredeploy.json" -TemplateParameterFile "C:\Users\Todd\Documents\GitHub\ArmDemo\cross-rg-vm\azuredeploy1.parameters.json"
 
